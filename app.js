bf8f9e67f530bcefe772dfb8e8d96262785df71b
datmaiq <129651293+datmaiq@users.noreply.github.com>
2024-05-27 23:30:40 +0700

    chore: remove README.md
    fix-commit
    
    fix-commit

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
-const indexRouter = require('./routes/index');
-const mongoose = require('mongoose');
-require('dotenv/config');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const indexRouter = require("./routes/index");
+const mongoose = require("mongoose");
+require("dotenv/config");
 
 const app = express();
 
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Connect to MONGODB
-mongoose.connect(process.env.MONGO_URI, () => {
-	console.log('Connected to Database!');
+app.use(express.static(path.join(__dirname, "public")));
+app._router.stack.forEach(function (r) {
+  if (r.route && r.route.path) {
+    console.log(r.route.path);
+  }
 });
 
-app.use('/', indexRouter);
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/development", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+  console.log(mongoose.connection.readyState);
+});
+
+app.use("/", indexRouter);
 
 module.exports = app;
--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -1,37 +1,103 @@
-const mongoose = require('mongoose');
-const Car = require('../models/Car');
+const { sendResponse, AppError } = require("../helpers/utils.js");
+
+const { Car, DB } = require("../models/Car.js");
+
 const carController = {};
 
 carController.createCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    const info = req.body;
+
+    if (!info) throw new AppError(402, "Bad Request", "Create car Error");
+    //mongoose query
+    const created = await Car.create(info);
+    console.log(created);
+    sendResponse(
+      res,
+      200,
+      true,
+      { car: created },
+      null,
+      "Create Car Successfully"
+    );
+  } catch (err) {
+    next(err);
+  }
 };
 
+//Get all car
 carController.getCars = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 20;
+  const skip = (page - 1) * limit;
+
+  try {
+    Car.f;
+    const listCar = await Car.find().skip(skip).limit(limit);
+    const total = await Car.countDocuments();
+
+    sendResponse(
+      res,
+      200,
+      true,
+      { cars: listCar, page: page, total: Math.ceil(total / limit) },
+      null,
+      "Get Car List Successfully!"
+    );
+  } catch (err) {
+    next(err);
+  }
 };
 
+//Update a car
 carController.editCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  const targetId = req.params.id;
+  const updateInfo = req.body;
+
+  if (!targetId || !updateInfo)
+    throw new AppError(402, "Bad Request", "Update Car Error");
+
+  const options = { new: true };
+  try {
+    const updated = await Car.findByIdAndUpdate(targetId, updateInfo, options);
+
+    if (!updated) throw new AppError(404, "Not Found", "Car not found");
+
+    sendResponse(
+      res,
+      200,
+      true,
+      { car: updated },
+      null,
+      "Update Car Successfully!"
+    );
+  } catch (err) {
+    next(err);
+  }
 };
 
+// Delete car
 carController.deleteCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  const targetId = req.params.id;
+
+  if (!targetId) throw new AppError(402, "Bad Request", "Delete Car Error");
+
+  try {
+    const deleted = await Car.findByIdAndDelete(targetId);
+
+    if (!deleted) throw new AppError(404, "Not Found", "Car not found");
+
+    sendResponse(
+      res,
+      200,
+      true,
+      { car: deleted },
+      null,
+      "Delete Car Successfully!"
+    );
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = carController;
