fb6883649663bc703e0888e14ed1fd44f29d53cc
Dat <datmaiq@gmail.com>
2024-05-28 00:06:11 +0700

    fix: use config env var instead of fixed value

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app._router.stack.forEach(function (r) {
 });
 
 // Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/development", {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
